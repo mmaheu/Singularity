aca14a52f65e4a8f9a0723d8331190299decf3de
Mike Maheu <mmaheu@24atoms.com>
2011-08-22 21:56:55 -0400

    whitespace

--- app/controllers/crms_controller.php
+++ app/controllers/crms_controller.php
@@ -1,10 +1,10 @@
 <?php
 class CrmsController extends AppController {
-	var $helpers = array ('Html','Form');
-	var $name = 'Crms';
+    var $helpers = array ('Html','Form');
+    var $name = 'Crms';
 
-	function index() {
-		$this->set('crms', $this->Crm->find('all'));
-	}
+function index() {
+    $this->set('crms', $this->Crm->find('all'));
+    }
 }
 ?>
